a9f1cea6e1230bbd53c68422f40cdf5774aa0f1f
Iago Cavalcante <iagoangelimc@gmail.com>
2017-05-02 09:43:15 -0300

    Teste

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function(req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send(req.query['hub.challenge'])
 })
 
 // for Facebook verification
