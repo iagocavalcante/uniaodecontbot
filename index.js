9e648746f49f0810ad89854db8d5664fc2f6afac
Iago Cavalcante <iagoangelimc@gmail.com>
2017-05-02 10:48:48 -0300

    Teste

--- index.js
+++ index.js
@@ -34,12 +34,13 @@ app.listen(app.get('port'), function() {
 
 app.post('/webhook/', function(req, res) {
     messaging_events = req.body.entry[0].messaging
+    console.log(messaging_events)
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
         sender = event.sender.id
         if (event.message && event.message.text) {
             text = event.message.text
-            if (text === 'hi') {
+            if (text === 'oi') {
                 sendGenericMessage(sender)
                 continue
             }
