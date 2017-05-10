0bf50de80a11de07970a19c976b20c51db242d16
Iago Cavalcante <iagoangelimc@gmail.com>
2017-05-09 21:42:09 -0300

    Mongo connect

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const processMessage = require('./controllers/processMessage')
 const getStarted = require('./controllers/gettingStarted')
 var mongoose = require('mongoose')
 
-const MONGO_HOST = (process.env.MONGO_HOST || 'heroku_bt4c199d:a1s2d3f4A!@ds133388.mlab.com:33388');
-app.set('mongo_url', (process.env.MONGODB_URL || 'mongodb://' + MONGO_HOST + '/heroku_bt4c199d'));
+// const MONGO_HOST = (process.env.MONGO_HOST || 'heroku_bt4c199d:a1s2d3f4A!@ds133388.mlab.com:33388');
+// app.set('mongo_url', (process.env.MONGODB_URL || 'mongodb://' + MONGO_HOST + '/heroku_bt4c199d'));
 app.set('port', (process.env.PORT || 5000))
 
-mongoose.connect(app.get('mongo_url'), (err) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    }
-    console.log("connected to " + app.get('mongo_url'));
-})
+// mongoose.connect(app.get('mongo_url'), (err) => {
+//     if (err) {
+//         console.error(err);
+//         process.exit(1);
+//     }
+//     console.log("connected to " + app.get('mongo_url'));
+// })
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
