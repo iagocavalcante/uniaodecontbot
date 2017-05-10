59befc2c133cce080ec6bd0ce0b277e8fb28be8f
Iago Cavalcante <iagoangelimc@gmail.com>
2017-05-09 22:20:31 -0300

    Mongo connect

--- controllers/gettingStarted.js
+++ controllers/gettingStarted.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const request = require('request')
 const FACEBOOK_ACCESS_TOKEN = 'EAARnZCjoA6J4BAJu4dabKK2M2ZBh1YJGAMRkSCfd9JLDZBtKa5CbmDzdjmRACm4m71VqJAzmyIn8fJZA3LeGCfON3asigZCvBJqql8OtDy6e6rmqLgMe8ENEMGIXC0HAyjwbZBrx581Om5d3R7queNCSdQxYti5lWM5Epyz4AugQZDZD';
 
@@ -142,4 +144,11 @@ function setupGetStartedButton(res) {
                 res.send(body);
             }
         });
+}
+
+module.exports = {
+    setupGetStartedButton,
+    register,
+    setupGreetingText,
+    setupPersistentMenu
 }
