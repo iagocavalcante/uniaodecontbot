66a1d7ec351a2d7e9ab639a458dbaafe36758288
Iago Cavalcante <iagoangelimc@gmail.com>
2017-05-09 21:44:42 -0300

    Mongo connect

--- controllers/gettingStarted.js
+++ controllers/gettingStarted.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const FACEBOOK_ACCESS_TOKEN = 'EAARnZCjoA6J4BAJu4dabKK2M2ZBh1YJGAMRkSCfd9JLDZBtKa5CbmDzdjmRACm4m71VqJAzmyIn8fJZA3LeGCfON3asigZCvBJqql8OtDy6e6rmqLgMe8ENEMGIXC0HAyjwbZBrx581Om5d3R7queNCSdQxYti5lWM5Epyz4AugQZDZD';
 
-module.exports = (res, FACEBOOK_ACCESS_TOKEN) => {
+module.exports = (res) => {
     let messageData = {
         "get_started": [{
             "payload": "USER_DEFINED_PAYLOAD"
