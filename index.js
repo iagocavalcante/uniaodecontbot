e2fdfd79ec751d63822d750c9f709b72c62aa770
Iago Cavalcante <iagoangelimc@gmail.com>
2017-05-16 19:28:18 -0300

    Teste

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const request = require('request')
 const app = express()
 const verification = require('./controllers/verification')
 const processMessage = require('./controllers/processMessage')
-let getStart = require('./controllers/gettingStarted')
-var mongoose = require('mongoose')
+const getStart = require('./controllers/gettingStarted')
+const mongoose = require('mongoose')
 
-// const MONGO_HOST = (process.env.MONGO_HOST || 'heroku_bt4c199d:a1s2d3f4A!@ds133388.mlab.com:33388');
-// app.set('mongo_url', (process.env.MONGODB_URL || 'mongodb://' + MONGO_HOST + '/heroku_bt4c199d'));
+const MONGO_HOST = (process.env.MONGO_HOST || 'teste:a1s2d3f4A!@ds133388.mlab.com:33388');
+app.set('mongo_url', (process.env.MONGODB_URL || 'mongodb://' + MONGO_HOST + '/heroku_bt4c199d'));
 app.set('port', (process.env.PORT || 5000))
 
-// mongoose.connect(app.get('mongo_url'), (err) => {
-//     if (err) {
-//         console.error(err);
-//         process.exit(1);
-//     }
-//     console.log("connected to " + app.get('mongo_url'));
-// })
+mongoose.connect(app.get('mongo_url'), (err) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log("connected to " + app.get('mongo_url'));
+})
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
